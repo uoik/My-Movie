bae4bdeaac443a63c88289a3a22b6082395934b3
web-小白 <809746386@qq.com>
2020-02-10 18:06:03 +0800

    项目完结

---
+++ server/public/build/precache-manifest.de04a896107cad1d6fd246ae0f3b46bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37116e074db2d208eed74784b3ddc860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7999d0b9879dc7fdca28",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "63d2a91eae417c7e9d33",
+    "url": "/static/css/main.c1e65eca.chunk.css"
+  },
+  {
+    "revision": "7999d0b9879dc7fdca28",
+    "url": "/static/js/2.98fcf2a2.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.98fcf2a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63d2a91eae417c7e9d33",
+    "url": "/static/js/main.ba2a0e21.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  },
+  {
+    "revision": "fa54d02e3139850d9798c93fdb25b8c8",
+    "url": "/static/media/no.fa54d02e.jpg"
+  }
+]);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import MovieRouter from './routes/MovieRoute';
 import UploadRouter from './routes/UploadRoute';
+import history from 'connect-history-api-fallback';
 
 const app = express();
 
@@ -11,6 +12,12 @@ app.use('/api/movie', MovieRouter);
 
 // 图片上传
 app.use('/api/upload', UploadRouter);
+
+// 中间件，用于通过指定的索引页代理请求
+app.use(history());
+
+// 页面
+app.use('/', express.static('public/build'));
 
 // 查找图片
 app.use('/upload', express.static('public/upload'));
